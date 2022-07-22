c05b27c3441dc367a317c45c0b054e9fe4de0050
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-21 21:51:01 -0700

    update generated html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -24,6 +24,7 @@ function generateHTML(data) {
         <title>Team Profile Generator</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
       </head>
       <body>
       <style>
