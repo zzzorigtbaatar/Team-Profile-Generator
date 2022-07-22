e2afb175c6be768e8f04a1cd751bd0d7df35dd0d
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-21 21:34:34 -0700

    add filter data in generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,9 +1,21 @@
-function generateHTML(data) {
-    console.log(data);
-    return `
-    <html>
-    </html>
-    `;
+function filterData(data) {
+    const manager = data.filter((employee) => {
+      return employee.getRole() === "Manager";
+    });
+    const engineers = data.filter((employee) => {
+      return employee.getRole() === "Engineer";
+    });
+    const interns = data.filter((employee) => {
+      return employee.getRole() === "Intern";
+    });
+    const arr = [manager, engineers, interns];
+    return arr;
   }
 
-  module.exports = generateHTML;
+function generateHTML(data) {
+    const filteredData = filterData(data);
+
+    return ``;
+}
+
+module.exports = generateHTML;
