0b4bc8e6de9e99de4a6e187dc1072c3184c87bd3
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-21 22:08:06 -0700

    add test for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,63 +3,71 @@ const Employee = require('../lib/Employee');
 describe("Employee class", () => {
 
   // Test for all use cases when initializing a new Employee object
-//   describe("Initialization", () => {
-//     it("should create an object with a name and age if provided valid arguments", () => {
-//       // Act
-//       const child = new Child("Sarah", 3);
+  describe("Initialization", () => {
+    it("should create an object with a name, id, and email if provided valid arguments", () => {
+      // Act
+      const employee = new Employee("Sarah", 322, "cat@gmail.com");
 
-//       // Verify that the new object has the correct properties
-//       expect(child.name).toEqual("Sarah");
-//       expect(child.age).toEqual(3);
-//     });
+      // Verify that the new object has the correct properties
+      expect(employee.name).toEqual("Sarah");
+      expect(employee.id).toEqual(322);
+      expect(employee.email).toEqual("cat@gmail.com");
+    });
 
-//     it("should throw an error if provided no arguments", () => {
-//       // Wrap the object initialization in a callback function that Jest will run
-//       // Act
-//       const cb = () => new Child();
+    describe("Get Role", () => {
+      it("should return 'Employee' when getRole() is called", () => {
+        const employee = new Employee("Sarah", 322, "cat@gmail.com");
+        expect(employee.getRole()).toEqual("Employee");
+      });
+    });
+  });
+  // it("should throw an error if provided no arguments", () => {
+  //   // Wrap the object initialization in a callback function that Jest will run
+  //   // Act
+  //   const cb = () => new Employee();
 
-//       // Verify that an error was thrown in the callback function
-//       // Assert
-//       expect(cb).toThrow();
-//     });
+  //   // Verify that an error was thrown in the callback function
+  //   // Assert
+  //   expect(cb).toThrow();
+  // });
 
-//     it("should throw an error if not provided an age", () => {
-//       // Act
-//       const cb = () => new Child("Sarah");
+  // it("should throw an error if not provided an age", () => {
+  //   // Act
+  //   const cb = () => new Employee("Sarah");
 
-//       // Define the error message that is expected to be thrown
-//       const err = new Error("Expected parameter 'age' to be a non-negative number");
+  //   // Define the error message that is expected to be thrown
+  //   const err = new Error("Expected parameter 'age' to be a non-negative number");
 
-//       // Verify that the correct error was thrown when the callback is executed
-//       // Assert
-//       expect(cb).toThrowError(err);
-//     });
+  //   // Verify that the correct error was thrown when the callback is executed
+  //   // Assert
+  //   expect(cb).toThrowError(err);
+  // });
 
-//     it("should throw an error if 'name' is not a string", () => {
-//       // Act
-//       const cb = () => new Child(3, 2);
-//       const err = new Error("Expected parameter 'name' to be a non-empty string");
+  // it("should throw an error if 'name' is not a string", () => {
+  //   // Act
+  //   const cb = () => new Employee(3, 2);
+  //   const err = new Error("Expected parameter 'name' to be a non-empty string");
 
-//       // Assert
-//       expect(cb).toThrowError(err);
-//     });
+  //   // Assert
+  //   expect(cb).toThrowError(err);
+  // });
 
-//     it("should throw an error if 'age' is not a number", () => {
-//       // Act
-//       const cb = () => new Child("Sarah", "2");
-//       const err = new Error("Expected parameter 'age' to be a non-negative number");
+  // it("should throw an error if 'age' is not a number", () => {
+  //   // Act
+  //   const cb = () => new Employee("Sarah", "2");
+  //   const err = new Error("Expected parameter 'age' to be a non-negative number");
 
-//       // Assert
-//       expect(cb).toThrowError(err);
-//     });
+  //   // Assert
+  //   expect(cb).toThrowError(err);
+  // });
 
-//     it("should throw an error if 'age' is less than 0", () => {
-//       // Act
-//       const cb = () => new Child("Sarah", -1);
-//       const err = new Error("Expected parameter 'age' to be a non-negative number");
+  // it("should throw an error if 'age' is less than 0", () => {
+  //   // Act
+  //   const cb = () => new Employee("Sarah", -1);
+  //   const err = new Error("Expected parameter 'age' to be a non-negative number");
 
-//       // Assert
-//       expect(cb).toThrowError(err);
-//     });
-//   });
+  //   // Assert
+  //   expect(cb).toThrowError(err);
+  // });
+
 }); 
